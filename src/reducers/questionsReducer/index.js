fe420599c89ff5cbde2450749c665d93d56dc8ef
tiago santos <xlzthyago@gmail.com>
2026-01-01 11:02:14 -0300

    freat: configurtion questionReducer

---
+++ src/reducers/questionsReducer/index.js
@@ -0,0 +1,16 @@
+
+const initialState ={
+    questions:[]
+}
+
+
+export const questionReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_QUESTION':
+            return {...state, questions: [...state.questions, action.payload]}
+    
+        default:
+            return state;
+    }
+
+}
