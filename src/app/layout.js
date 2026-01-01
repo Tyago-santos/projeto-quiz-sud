578ebe500a777d00dd46925e0a3c824e5e143b26
tiago santos <xlzthyago@gmail.com>
2026-01-01 11:25:25 -0300

    feat:  inset configuration reducer and contex

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,26 +2,29 @@
 'use client'
 import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+
+
+import { createContext, useContext, useReducer } from 'react';
+
+import { rootReducers,initialStateReducers } from '@/reducers';
 
 // import {  useRouter} from 'next/navigation'
-
+export const ProviderContext = createContext(); 
 
 export default function RootLayout({ children }) {
 
-    // const isAuth = false;
-    // const router = useRouter();
-
-    // if (!isAuth) {
-    //   router.push('/login');
-    // }
-
+  const [state, dispatch] = useReducer(rootReducers, initialStateReducers);
+  
 
   return (
+    <ProviderContext.Provider value={[state, dispatch]}>
     <html lang="pt-BR" className={inter.className}>
       <body>
         {children}
       </body>
     </html>
+
+    </ProviderContext.Provider>
   )
 }
