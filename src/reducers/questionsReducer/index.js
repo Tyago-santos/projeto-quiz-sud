f1f83053b65c81a696289b32cf07afd019d0a5f0
tiago santos <xlzthyago@gmail.com>
2026-01-10 18:39:11 -0300

    "chore: implement reduce of lesson"

--- src/reducers/questionsReducer/index.js
+++ src/reducers/questionsReducer/index.js
@@ -1,17 +1,23 @@
 
 export const initialStateQuestion ={
-    categorySelect:[]
+    categorySelect:[],
+    lesson:[]
 }
 
 
 export const questionReducer = (state = initialStateQuestion, action) => {
     switch (action.type) {
-        case 'ADD_QUESTION':
+        case 'ADD_QUESTION_SELECT':
             return {...state, categorySelect: [...state.categorySelect, action.payload]}
     
-        case 'REMOVE_QUESTION':
+        case 'REMOVE_QUESTION_SELECT':
             const newState =  state.categorySelect.filter(item => item  !== action.payload )
             return {...state, categorySelect: [...newState]}
+
+
+        case 'ADD_LESSON':
+            return {...state, lesson:  [...action.payload]}
+    
     
         default:
             return state;
